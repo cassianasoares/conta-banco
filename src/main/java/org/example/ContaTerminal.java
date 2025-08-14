f155f84d455d3c67e2e73d9d061ed6d8baba2408
cassianasoares <cassianassilva@hotmail.com>
2025-08-14 14:42:29 -0300

    feat: add basic ContaTerminal class

---
+++ src/main/java/org/example/ContaTerminal.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da conta:");
+        int numeroConta = scanner.nextInt();
+        System.out.println("Por favor, digite o número da agência:");
+        String agencia = scanner.next();
+        System.out.println("Por favor, digite o nome do cliente:");
+        String nomeCliente = scanner.next();
+        System.out.println("Por favor, digite o saldo da conta:");
+        double saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+    }
+}
